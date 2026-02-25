88427f1b53f41a138e01605729a3b2ad44b8f820
Fran López <franlopezpineda@gmail.com>
2026-02-25 12:45:31 +0100

    correcciones

--- backend/src/main/java/com/ingenieria/repository/LocalRepository.java
+++ backend/src/main/java/com/ingenieria/repository/LocalRepository.java
@@ -3,7 +3,6 @@ package com.ingenieria.repository;
 import com.ingenieria.model.Local;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import org.springframework.data.jpa.repository.EntityGraph;
 import java.util.List;
 
 import java.util.Optional;
